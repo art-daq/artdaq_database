207adfaf8be02c9023570e3c3cff3ccea6dfd2bd
lukhanin <lukhanin@fnal.gov>
2016-10-03 14:05:32 -0500

    Replace json parser for filesystemdb

--- artdaq-database/JsonDocument/JSONDocument_utils.cpp
+++ artdaq-database/JsonDocument/JSONDocument_utils.cpp
@@ -1,8 +1,7 @@
 #include "artdaq-database/JsonDocument/JSONDocument.h"
 
+#include "artdaq-database/DataFormats/Json/json_common.h"
 #include "artdaq-database/JsonDocument/common.h"
-
-#include <bsoncxx/json.hpp>
 
 #ifdef TRACE_NAME
 #undef TRACE_NAME
@@ -10,39 +9,48 @@
 
 #define TRACE_NAME "JSNU:Document_C"
 
-namespace artdaq {
-namespace database {
-namespace mongo {
-std::string to_json(bsoncxx::document::view view);
-}  // namespace mongo
-}  // namespace database
-}  // namespace artdaq
-
-
-namespace adj= artdaq::database::jsonutils;
+namespace adj = artdaq::database::jsonutils;
+namespace jsn = artdaq::database::json;
 
 bool adj::isValidJson(std::string const& json_buffer) {
-    try {
-        auto document = bsoncxx::from_json(json_buffer);
-    } catch(std::exception const& e) {
-        TRACE_(10, "isValidJson()" << "Caught exception:" << e.what() );
-        return false;
-    }
-    return true;
+  assert(!json_buffer.empty());
+
+  jsn::object_t doc_ast1;
+
+  if (!jsn::JsonReader{}.read(json_buffer, doc_ast1)) {
+    TRACE_(10, "Failed reading JSON buffer: << " << json_buffer << ">>\n");
+    return false;
+  }
+
+  return true;
 }
 
-std::string adj::filterJson(std::string const& json) {
-    assert(!json.empty());
+std::string adj::filterJson(std::string const& json_buffer) {
+  assert(!json_buffer.empty());
 
-    TRACE_(10, "filterJson() json=<" << json << ">");
+  TRACE_(10, "filterJson() json=<" << json_buffer << ">");
 
-    try {
-        auto document = bsoncxx::from_json(json);
-        return  artdaq::database::mongo::to_json(document.view());
+  try {
+    jsn::object_t doc_ast1;
+    if (!jsn::JsonReader{}.read(json_buffer, doc_ast1)) {
+      TRACE_(10, "Failed reading JSON buffer: << " << json_buffer << ">>\n");
+      throw cet::exception("JSONDocument_utils") << "Failed reading JSON buffer: << " << json_buffer << ">>\n";
     }
-    catch(std::exception const& e) {
-        TRACE_(10, "filterJson()"<< "Caught exception:" << e.what() );
 
-        throw cet::exception("JSONDocument_utils") << "Caught exception:" << e.what();
+    if(doc_ast1.empty())
+      return json_buffer;
+    
+    auto jsonout = std::string();
+
+    if (!jsn::JsonWriter{}.write(doc_ast1, jsonout)) {
+      TRACE_(10, "Failed writing JSON AST: << " << json_buffer << ">>\n");
+      throw cet::exception("JSONDocument_utils") << "Failed writing JSON buffer: << " << json_buffer << ">>\n";
     }
+    return jsonout;
+  } catch (std::exception const& e) {
+    TRACE_(10, "filterJson()"
+                   << "Caught exception:" << e.what());
+
+    throw cet::exception("JSONDocument_utils") << "Caught exception:" << e.what();
+  }
 }
