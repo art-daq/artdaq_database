a156bc0140136cb0c3eb5947a655f3c0a0a76042
lukhanin <lukhanin@fnal.gov>
2016-10-20 13:05:57 -0500

    Add PYTHONPATH to the table file

--- artdaq-database/SWIGBindings/python/conftool/conftool.h
+++ artdaq-database/SWIGBindings/python/conftool/conftool.h
@@ -3,13 +3,13 @@
 
 typedef std::pair<bool, std::string> result_pair_t;
 
-result_pair_t load_configuration_ui(std::string const& search_filter);
-result_pair_t store_configuration_ui(std::string const& search_filter, std::string const& json_document);
-result_pair_t find_global_configurations_ui(std::string const& search_filter);
-result_pair_t build_global_configuration_search_filter_ui(std::string const& search_filter);
-result_pair_t add_configuration_to_global_configuration_ui(std::string const& search_filter);
-result_pair_t create_new_global_configuration_ui(std::string const& search_filter);
-result_pair_t find_configuration_versions_ui(std::string const& search_filter);
-result_pair_t find_configuration_entities_ui(std::string const& search_filter);
-result_pair_t load_globalconfiguration_ui(std::string const& search_filter);
-result_pair_t store_globalconfiguration_ui(std::string const& search_filter, std::string const& tarbz2base64);
+result_pair_t load_configuration(std::string const& search_filter);
+result_pair_t store_configuration(std::string const& search_filter, std::string const& json_document);
+result_pair_t find_global_configurations(std::string const& search_filter);
+result_pair_t build_global_configuration_search_filter(std::string const& search_filter);
+result_pair_t add_configuration_to_global_configuration(std::string const& search_filter);
+result_pair_t create_new_global_configuration(std::string const& search_filter);
+result_pair_t find_configuration_versions(std::string const& search_filter);
+result_pair_t find_configuration_entities(std::string const& search_filter);
+result_pair_t load_globalconfiguration(std::string const& search_filter);
+result_pair_t store_globalconfiguration(std::string const& search_filter, std::string const& tarbz2base64);
