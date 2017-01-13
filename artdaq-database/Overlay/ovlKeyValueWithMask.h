7be8d03055a5bb96fd2b4553fe7f00d3b7a5d177
lukhanin <lukhanin@fnal.gov>
2017-01-13 15:49:48 -0600

    wip: synchronized json field names

---
+++ artdaq-database/Overlay/ovlKeyValueWithMask.h
@@ -0,0 +1,40 @@
+#ifndef _ARTDAQ_DATABASE_OVERLAY_OVLKEYVALUEWMASK_H_
+#define _ARTDAQ_DATABASE_OVERLAY_OVLKEYVALUEWMASK_H_
+
+#include "artdaq-database/Overlay/common.h"
+#include "artdaq-database/Overlay/ovlKeyValue.h"
+
+namespace artdaq {
+namespace database {
+namespace overlay {
+namespace jsonliteral = artdaq::database::dataformats::literal;
+using namespace artdaq::database::result;
+using artdaq::database::sharedtypes::unwrap;
+
+template <int mask>
+class ovlKeyValueWithMask : public ovlKeyValue {
+ public:
+  ovlKeyValueWithMask(object_t::key_type const& /*key*/, value_t& /*object*/);
+
+  // defaults
+  ovlKeyValueWithMask(ovlKeyValueWithMask&&) = default;
+  ~ovlKeyValueWithMask() = default;
+
+  // ops
+  result_t operator==(ovlKeyValueWithMask const&) const;
+};
+
+template <int mask>
+ovlKeyValueWithMask<mask>::ovlKeyValueWithMask(object_t::key_type const& key, value_t& value)
+    : ovlKeyValue(key, value) {}
+
+template <int mask>
+result_t ovlKeyValueWithMask<mask>::operator==(ovlKeyValueWithMask const& other) const {
+  return ((useCompareMask() & mask) == mask) ? Success() : self() == other.self();
+}
+
+}  // namespace overlay
+}  // namespace database
+}  // namespace artdaq
+
+#endif /* _ARTDAQ_DATABASE_OVERLAY_OVLKEYVALUEWMASK_H_ */
