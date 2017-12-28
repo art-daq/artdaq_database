19e158173ebe5217ba3d67b18c6d8096ded08bd8
Gennadiy <lukhanin@fnal.gov>
2017-12-28 11:55:03 -0600

    integrate fhiclcpp patch

--- built-in/BuiltinFHICLCPP.cmake
+++ built-in/BuiltinFHICLCPP.cmake
@@ -17,4 +17,22 @@ endforeach()
 
 set(FHICLCPP artdaq-database_Fhiclcpp)
 
+find_path(BUILTIN_FHICLCPP_DIR ${TOP_CMAKE_SOURCE_DIR}/built-in/fhicl-cpp)
+
+if ( (NOT BUILTIN_FHICLCPP_DIR) OR (NOT EXISTS ${BUILTIN_FHICLCPP_DIR}))
+    message("Downloading fhicl-cpp source")
+
+    execute_process(COMMAND rm -rf ${TOP_CMAKE_SOURCE_DIR}/built-in/fhicl-cpp
+		    WORKING_DIRECTORY  ${TOP_CMAKE_SOURCE_DIR}/built-in)
+    
+    execute_process(COMMAND git clone http://cdcvs.fnal.gov/projects/fhicl-cpp ${TOP_CMAKE_SOURCE_DIR}/built-in/fhicl-cpp
+                    WORKING_DIRECTORY  ${TOP_CMAKE_SOURCE_DIR}/built-in)
+
+    execute_process(COMMAND  git checkout tags/v4_05_01 
+		    WORKING_DIRECTORY  ${TOP_CMAKE_SOURCE_DIR}/built-in/fhicl-cpp)
+		    
+    execute_process(COMMAND  git apply ${TOP_CMAKE_SOURCE_DIR}/built-in/fhicl-cpp.patch
+                    WORKING_DIRECTORY  ${TOP_CMAKE_SOURCE_DIR}/built-in/fhicl-cpp)                    
+endif()
+
 add_subdirectory(built-in)
