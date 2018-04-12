0a76be074541d698bdaba195b6021f322256e6b5
Gennadiy <lukhanin@fnal.gov>
2018-04-12 14:24:35 -0500

    update product versions

--- artdaq-database/deployment-scripts/install_artdaq_database.sh
+++ artdaq-database/deployment-scripts/install_artdaq_database.sh
@@ -5,8 +5,8 @@
 # Main program starts at the bottom of this file.
 #----------------------------------------------------------------
 ARTDAQ_UPS_QUAL="e14:prof:s50"
-ARTDAQ_DB_UPS_VER=v1_04_39
-WEBEDITOR_UPS_VER=v1_00_11
+ARTDAQ_DB_UPS_VER=v1_04_48
+WEBEDITOR_UPS_VER=v1_00_14
 
 ARTDAQ_BASE_DIR=/tmp/artdaq
 ARTDAQ_DB_NAME=cern_pddaq_v3x_db
